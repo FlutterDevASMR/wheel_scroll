b429a3acd2db5dd348167bb68c91948d0203e980
FlutterDevASMR <flutterdevasmr@gmail.com>
2023-07-15 20:44:26 +0330

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Row(
+          children: [
+            Expanded(
+              child: ListWheelScrollView.useDelegate(
+                itemExtent: 90,
+                childDelegate: ListWheelChildBuilderDelegate(
+                  builder: (context, index) => Text(
+                    (index < 10) ? '0$index' : index.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 80,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  childCount: 24,
+                ),
+                physics: FixedExtentScrollPhysics(),
+                perspective: 0.01,
+                diameterRatio: 1.7,
+              ),
+            ),
+            Text(
+              ':',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Expanded(
+              child: ListWheelScrollView.useDelegate(
+                itemExtent: 90,
+                childDelegate: ListWheelChildBuilderDelegate(
+                  builder: (context, index) => Text(
+                    (index < 10) ? '0$index' : index.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 80,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  childCount: 60,
+                ),
+                physics: FixedExtentScrollPhysics(),
+                perspective: 0.01,
+                diameterRatio: 1.7,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
